8dbb8c360b03887a1f687b5bbb05bf99f32bf230
Aaron Stevens <bheklilr2@gmail.com>
2017-08-23 21:21:45 -0500

    #2 Reverted something that I didn't like from yapf

--- samestats/samestats.py
+++ samestats/samestats.py
@@ -355,10 +355,7 @@ def get_points_for_shape(line_shape):
         l1 = [[cx, cy], [cx, cy]]
         lines = [l1]
     elif line_shape == 'star':
-        star_pts = [
-            10, 40, 40, 40, 50, 10, 60, 40, 90, 40, 65, 60, 75, 90, 50, 70, 25,
-            90, 35, 60
-        ]
+        star_pts = [10, 40, 40, 40, 50, 10, 60, 40, 90, 40, 65, 60, 75, 90, 50, 70, 25, 90, 35, 60]
         pts = [star_pts[i:i + 2] for i in range(0, len(star_pts), 2)]
         pts = [[p[0] * 0.8 + 20, 100 - p[1]] for p in pts]
         pts.append(pts[0])

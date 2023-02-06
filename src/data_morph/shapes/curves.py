2e38b2131ca5199454d13520ae69e294eaaa24e1
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2023-02-06 18:36:51 -0500

    Add curves.

---
+++ src/data_morph/shapes/curves.py
@@ -0,0 +1,21 @@
+"""Shapes that are composed of curves."""
+
+import numpy as np
+
+from .lines import Lines
+
+
+class DownParab(Lines):
+    """Class for the down parabola shape."""
+
+    def __init__(self, data) -> None:
+        q1, q3 = data.y.quantile([0.25, 0.75])
+
+        # TODO: figure out how to use the data to derive these
+        curve = [[x, -(((x - 50) / 4) ** 2) + 90] for x in np.arange(0, 100, 3)]
+
+        super().__init__(*[curve[i : i + 2] for i in range(0, len(curve) - 1, 1)])
+
+    def __repr__(self) -> str:
+        """Return string representation of the shape."""
+        return 'down_parab'

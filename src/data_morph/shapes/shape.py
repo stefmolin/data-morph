f6d20d3683c0c05473fe8fb195bf1718d9d2af63
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2023-02-06 17:51:27 -0500

    Add abstract base class for shapes.

---
+++ src/data_morph/shapes/shape.py
@@ -0,0 +1,22 @@
+"""Abstract base class for shapes."""
+
+from abc import ABC
+
+from scipy.spatial import distance
+
+
+class Shape(ABC):
+    """Abstract base class for a shape."""
+
+    def __init__(self, *args, **kwargs) -> None:
+        raise NotImplementedError
+
+    def __repr__(self) -> str:
+        return self.__class__.__name__.lower()
+
+    def distance(self, x, y) -> float:
+        raise NotImplementedError
+
+    @staticmethod
+    def _euclidean_distance(a, b) -> float:
+        return distance.euclidean(a, b)

29ebbf24d898a61de494d8d43e6e3f1616d915ec
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2023-02-06 18:22:23 -0500

    Add hlines to patterns.

--- src/data_morph/shapes/patterns.py
+++ src/data_morph/shapes/patterns.py
@@ -3,6 +3,22 @@
 from .lines import Lines
 
 
+class HorizontalLines(Lines):
+    """Class for the horizontal lines shape."""
+
+    def __init__(self, data) -> None:
+        # xmin, ymin = data.min()[['x', 'y']]
+        # xmax, ymax = data.max()[['x', 'y']]
+
+        super().__init__(
+            *[[[0, y], [100, y]] for y in [10, 30, 50, 70, 90]]
+        )  # TODO: figure out the values based on the data
+
+    def __repr__(self) -> str:
+        """Return string representation of the shape."""
+        return 'h_lines'
+
+
 class XLines(Lines):
     """Class for the X shape consisting of two crossing, perpendicular lines."""
 

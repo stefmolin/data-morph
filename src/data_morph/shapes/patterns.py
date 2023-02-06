eb5895d916e456c5a542455d5051fedccfab7b60
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2023-02-06 18:23:45 -0500

    Add wide lines to patterns.

--- src/data_morph/shapes/patterns.py
+++ src/data_morph/shapes/patterns.py
@@ -35,6 +35,21 @@ class VerticalLines(Lines):
         return 'v_lines'
 
 
+class WideLines(Lines):
+    """Class for the wide lines shape."""
+
+    def __init__(self, data) -> None:
+        q1, q3 = data.x.quantile([0.25, 0.75])
+
+        super().__init__(
+            [[q1, 0], [q1, 100]], [[q3, 0], [q3, 100]]
+        )  # TODO: figure out way to get 0, 100 min/max plus offset?
+
+    def __repr__(self) -> str:
+        """Return string representation of the shape."""
+        return 'wide_lines'
+
+
 class XLines(Lines):
     """Class for the X shape consisting of two crossing, perpendicular lines."""
 

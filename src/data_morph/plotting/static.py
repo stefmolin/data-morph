f8e692977c298aa0c57bc58b92fe549bfd12839f
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2023-02-11 18:31:22 -0500

    Clean up text.

--- src/data_morph/plotting/static.py
+++ src/data_morph/plotting/static.py
@@ -32,8 +32,8 @@ def plot(df: pd.DataFrame, save_to: str, decimals: int, **save_kwds) -> None:
     ax.set(xlim=(0, 105), ylim=(y_offset, 105))
 
     res = get_values(df)
-    fs = 30
 
+    locs = [80, 65, 50, 35, 20]
     labels = ('X Mean', 'Y Mean', 'X SD', 'Y SD', 'Corr.')
     max_label_length = max([len(label) for label in labels])
 
@@ -49,29 +49,19 @@ def plot(df: pd.DataFrame, save_to: str, decimals: int, **save_kwds) -> None:
     ).format
     stat_clip = visible_decimals - decimals
 
-    opts = dict(fontsize=fs, alpha=0.3)
-    ax.text(110, y_offset + 80, formatter(labels[0], res.x_mean), **opts)
-    ax.text(110, y_offset + 65, formatter(labels[1], res.y_mean), **opts)
-    ax.text(110, y_offset + 50, formatter(labels[2], res.x_stdev), **opts)
-    ax.text(110, y_offset + 35, formatter(labels[3], res.y_stdev), **opts)
-    ax.text(
-        110,
-        y_offset + 20,
-        corr_formatter(labels[4], res.correlation, pad=max_label_length),
-        **opts,
-    )
+    for label, loc, stat in zip(labels[:-1], locs, res):
+        ax.text(110, y_offset + loc, formatter(label, stat), fontsize=30, alpha=0.3)
+        ax.text(110, y_offset + loc, formatter(label, stat)[:-stat_clip], fontsize=30)
 
-    opts['alpha'] = 1
-    ax.text(110, y_offset + 80, formatter(labels[0], res.x_mean)[:-stat_clip], **opts)
-    ax.text(110, y_offset + 65, formatter(labels[1], res.y_mean)[:-stat_clip], **opts)
-    ax.text(110, y_offset + 50, formatter(labels[2], res.x_stdev)[:-stat_clip], **opts)
-    ax.text(110, y_offset + 35, formatter(labels[3], res.y_stdev)[:-stat_clip], **opts)
-    ax.text(
-        110,
-        y_offset + 20,
-        corr_formatter(labels[4], res.correlation, pad=max_label_length)[:-stat_clip],
-        **opts,
-    )
+    correlation_str = corr_formatter(labels[-1], res.correlation, pad=max_label_length)
+    for alpha, text in zip([0.3, 1], [correlation_str, correlation_str[:-stat_clip]]):
+        ax.text(
+            110,
+            y_offset + locs[-1],
+            text,
+            fontsize=30,
+            alpha=alpha,
+        )
 
     if not save_to:
         return ax

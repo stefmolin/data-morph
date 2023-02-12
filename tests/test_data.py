6c293cd58b691b0d8ff6634e8c6caf698535ff3b
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2023-02-12 08:37:22 -0500

    Switch to fixture.

--- tests/test_data.py
+++ tests/test_data.py
@@ -2,22 +2,28 @@
 
 import os
 
+import pytest
+
 from data_morph.data.loader import DataLoader
 
-DATASETS_DIR = os.path.join(
-    os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
-    'src',
-    'data_morph',
-    'data',
-    'datasets',
-)
+
+@pytest.fixture
+def datasets_dir(request):
+    """Fixture for the datasets directory."""
+    return os.path.join(
+        request.config.rootdir,
+        'src',
+        'data_morph',
+        'data',
+        'datasets',
+    )
 
 
-def test_data_load():
+def test_data_load(datasets_dir):
     """Confirm that loading the dataset by name and by path work."""
     loader = DataLoader([0, 100])
     dino_from_pkg = loader.load_dataset('dino')
-    dino_from_file = loader.load_dataset(os.path.join(DATASETS_DIR, 'dino.csv'))
+    dino_from_file = loader.load_dataset(os.path.join(datasets_dir, 'dino.csv'))
 
     assert dino_from_pkg[0] == dino_from_file[0]
     assert dino_from_pkg[1].equals(dino_from_file[1])

f86f489105516bb6d0b464b4ca6633a930003303
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2023-02-12 14:15:41 -0500

    Make a fixture for the factory.

--- tests/test_shapes.py
+++ tests/test_shapes.py
@@ -13,11 +13,14 @@ def sample_data():
     return pd.DataFrame({'x': [10, 20, 30], 'y': [50, 50, 80]})
 
 
-def test_shape_factory(sample_data):
+@pytest.fixture
+def shape_factory(sample_data):
+    """Fixture for a ShapeFactory of sample data."""
+    return ShapeFactory(sample_data)
+
+
+def test_shape_factory(shape_factory):
     """Test the ShapeFactory class."""
-
-    shape_factory = ShapeFactory(sample_data)
-
     for shape_name, shape_type in shape_factory.AVAILABLE_SHAPES.items():
         shape = shape_factory.generate_shape(shape_name)
         assert isinstance(shape, shape_type)
@@ -40,19 +43,19 @@ def test_shape_abc():
         NewShape().distance()
 
 
-def test_circle(sample_data):
+def test_circle(shape_factory):
     """Test the Circle class."""
-    circle = ShapeFactory(sample_data).generate_shape('circle')
+    circle = shape_factory.generate_shape('circle')
     assert circle.distance(20, 50) == 20.0
 
 
-def test_bullseye(sample_data):
+def test_bullseye(shape_factory):
     """Test the Bullseye class."""
-    bullseye = ShapeFactory(sample_data).generate_shape('bullseye')
+    bullseye = shape_factory.generate_shape('bullseye')
     assert bullseye.distance(20, 50) == 8.0
 
 
-def test_dots(sample_data):
+def test_dots(shape_factory):
     """Test the Dots class."""
-    dots = ShapeFactory(sample_data).generate_shape('dots')
+    dots = shape_factory.generate_shape('dots')
     assert dots.distance(20, 50) == 0.0

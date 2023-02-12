649c706fdbd7a46c4910984c1267c4700b16e989
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2023-02-12 15:25:46 -0500

    Move sample data to fixture in conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+"""Pytest config."""
+
+import pandas as pd
+import pytest
+
+
+@pytest.fixture(scope='session', autouse=True)
+def sample_data():
+    """Fixture for the sample data."""
+    return pd.DataFrame({'x': [10, 20, 30], 'y': [50, 50, 80]})

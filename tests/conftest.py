34c106db7d60adce46338cd1c03ac623a9aa3448
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2023-02-18 13:29:09 -0500

    Update session fixture to no longer mark autouse for sample_data.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ import pandas as pd
 import pytest
 
 
-@pytest.fixture(scope='session', autouse=True)
+@pytest.fixture(scope='session')
 def sample_data():
     """Fixture for the sample data."""
     return pd.DataFrame({'x': [10, 20, 30], 'y': [50, 50, 80]})

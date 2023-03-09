afbeb48d0dbe61ddb354b2d66c24b3081262ba0d
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2023-03-08 20:54:50 -0500

    Switch to tmp_path fixture.

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -1,7 +1,5 @@
 """Tests for the data_morph.plotting subpackage."""
 
-import os
-
 import numpy as np
 import pytest
 
@@ -10,12 +8,11 @@ from data_morph.plotting.static import plot
 
 
 @pytest.mark.parametrize('file_path', ['test_plot.png', None])
-def test_plot(sample_data, file_path):
+def test_plot(sample_data, tmp_path, file_path):
     """Test static plot creation."""
     bounds = (-5.0, 105.0)
     if file_path:
-        temp_dir = 'does-not-exist'
-        save_to = os.path.join(temp_dir, file_path)
+        save_to = tmp_path / 'another-level' / file_path
 
         plot(
             df=sample_data,
@@ -24,11 +21,8 @@ def test_plot(sample_data, file_path):
             save_to=save_to,
             decimals=2,
         )
-        assert os.path.isfile(save_to)
+        assert save_to.is_file()
 
-        # clean up
-        os.remove(save_to)
-        os.rmdir(temp_dir)
     else:
         ax = plot(
             df=sample_data, x_bounds=bounds, y_bounds=bounds, save_to=None, decimals=2
@@ -42,7 +36,7 @@ def test_plot(sample_data, file_path):
         assert ax.get_ylim() == bounds
 
 
-def test_frame_stitching(sample_data, tmpdir):
+def test_frame_stitching(sample_data, tmp_path):
     """Test stitching frames into a GIF animation."""
     start_shape = 'sample'
     target_shape = 'circle'
@@ -53,22 +47,18 @@ def test_frame_stitching(sample_data, tmpdir):
             df=sample_data + np.random.randn(),
             x_bounds=bounds,
             y_bounds=bounds,
-            save_to=os.path.join(
-                tmpdir, f'{start_shape}-to-{target_shape}-{frame}.png'
-            ),
+            save_to=(tmp_path / f'{start_shape}-to-{target_shape}-{frame}.png'),
             decimals=2,
         )
 
     stitch_gif_animation(
-        output_dir=tmpdir,
+        output_dir=tmp_path,
         start_shape=start_shape,
         target_shape=target_shape,
         keep_frames=False,
         forward_only_animation=False,
     )
 
-    animation_file = os.path.join(tmpdir, f'{start_shape}_to_{target_shape}.gif')
-    assert os.path.isfile(animation_file)
-    assert not os.path.isfile(
-        os.path.join(tmpdir, f'{start_shape}-to-{target_shape}-{frame}.png')
-    )
+    animation_file = tmp_path / f'{start_shape}_to_{target_shape}.gif'
+    assert animation_file.is_file()
+    assert not (tmp_path / f'{start_shape}-to-{target_shape}-{frame}.png').is_file()

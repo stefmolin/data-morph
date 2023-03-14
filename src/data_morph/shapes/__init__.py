021346efd93330fce0e927ecce6f15c521b0e961
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2023-03-13 23:20:41 -0400

    Improve shape module docstring.

--- src/data_morph/shapes/__init__.py
+++ src/data_morph/shapes/__init__.py
@@ -1,1 +1,6 @@
-"""Classes for specific shapes that data can be morphed into."""
+"""
+Classes for specific shapes that data can be morphed into.
+
+The :class:`.ShapeFactory` is used to generate shape objects.
+Shapes are grouped into modules based on composition.
+"""

8e1a25f82583584e95afd924deedcaf49c4e6dae
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2023-03-19 19:05:50 -0400

    Move DotsGrid to points module.

--- src/data_morph/shapes/points.py
+++ src/data_morph/shapes/points.py
@@ -1,9 +1,47 @@
 """Shapes that are composed of points."""
 
+import itertools
+
 import numpy as np
 
 from ..data.dataset import Dataset
 from .bases.point_collection import PointCollection
+
+
+class DotsGrid(PointCollection):
+    """
+    Class representing a 3x3 grid of dots.
+
+    .. plot::
+       :scale: 75
+       :caption:
+            This shape is generated using the dino dataset
+            (without normalization).
+
+        from data_morph.data.loader import DataLoader
+        from data_morph.shapes.patterns import DotsGrid
+
+        _ = DotsGrid(DataLoader.load_dataset('dino')).plot()
+
+    Parameters
+    ----------
+    dataset : Dataset
+        The starting dataset to morph into other shapes.
+    """
+
+    def __init__(self, dataset: Dataset) -> None:
+        xlow, xhigh = dataset.df.x.quantile([0.05, 0.95]).tolist()
+        ylow, yhigh = dataset.df.y.quantile([0.05, 0.95]).tolist()
+
+        xmid = (xhigh + xlow) / 2
+        ymid = (yhigh + ylow) / 2
+
+        super().__init__(
+            *list(itertools.product([xlow, xmid, xhigh], [ylow, ymid, yhigh]))
+        )
+
+    def __str__(self) -> str:
+        return 'dots'
 
 
 class DownParabola(PointCollection):

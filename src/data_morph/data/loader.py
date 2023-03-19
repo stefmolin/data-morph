f615e58afd6fe285f4e9aa32b75fc1af264d9547
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2023-03-19 14:57:11 -0700

    Enable music dataset as a starter shape.

--- src/data_morph/data/loader.py
+++ src/data_morph/data/loader.py
@@ -17,6 +17,7 @@ class DataLoader:
     _DATA_PATH: str = 'data/starter_shapes/'
     _DATASETS: dict = {
         'dino': 'dino.csv',
+        'music': 'music.csv',
         'panda': 'panda.csv',
     }
     AVAILABLE_DATASETS = list(_DATASETS.keys())

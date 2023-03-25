e4b50e2db91ee3026ac10fc5a998b7366ac06920
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2023-03-24 20:27:32 -0400

    Add rectangle shape.

--- src/data_morph/shapes/polygons.py
+++ src/data_morph/shapes/polygons.py
@@ -4,6 +4,37 @@ from ..data.dataset import Dataset
 from .bases.line_collection import LineCollection
 
 
+class Rectangle(LineCollection):
+    """
+    Class for the rectangle shape.
+
+    Parameters
+    ----------
+    dataset : Dataset
+        The starting dataset to morph into other shapes.
+    """
+
+    def __init__(self, dataset: Dataset) -> None:
+        xmin, xmax = dataset.data_bounds.x_bounds
+        ymin, ymax = dataset.data_bounds.y_bounds
+
+        x_offset = dataset.data_bounds.x_bounds.range / 5
+        y_offset = dataset.data_bounds.y_bounds.range / 5
+
+        xmin += x_offset
+        xmax -= x_offset
+
+        ymin += y_offset
+        ymax -= y_offset
+
+        super().__init__(
+            [[xmin, ymin], [xmin, ymax]],
+            [[xmin, ymin], [xmax, ymin]],
+            [[xmax, ymin], [xmax, ymax]],
+            [[xmin, ymax], [xmax, ymax]],
+        )
+
+
 class Star(LineCollection):
     """
     Class for the star shape.

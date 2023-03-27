123fa38b9dbb9ae640f670cdcb73106e223b9df5
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2023-03-27 13:37:50 -0400

    Reorg data tests into separate files using test classes for grouping; create dataset marker.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 """Pytest config."""
 
+from pathlib import Path
+
 import pandas as pd
 import pytest
 
@@ -8,3 +10,11 @@ import pytest
 def sample_data():
     """Fixture for the sample data."""
     return pd.DataFrame({'x': [10, 20, 30], 'y': [50, 50, 80]})
+
+
+@pytest.fixture(scope='session')
+def starter_shapes_dir(request):
+    """Fixture for the starter shapes directory."""
+    return (
+        Path(request.config.rootdir) / 'src' / 'data_morph' / 'data' / 'starter_shapes'
+    )

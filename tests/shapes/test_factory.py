87cff935eab930e585318617ae9e1e7ad7cf3d0a
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2023-03-27 22:52:11 -0400

    Split out shape factory tests.

--- tests/shapes/test_factory.py
+++ tests/shapes/test_factory.py
@@ -8,11 +8,13 @@ class TestShapeFactory:
     """Test the ShapeFactory class."""
 
     def test_generate_shape(self, shape_factory):
-        """Test the ShapeFactory class."""
+        """Test the generate_shape() method on a valid shape."""
         for shape_name, shape_type in shape_factory._SHAPE_MAPPING.items():
             shape = shape_factory.generate_shape(shape_name)
             assert isinstance(shape, shape_type)
             assert shape_name == str(shape)
 
+    def test_generate_shape_error(self, shape_factory):
+        """Test the generate_shape() method on a non-existent shape."""
         with pytest.raises(ValueError, match='No such shape'):
             _ = shape_factory.generate_shape('does not exist')

8be090e59c5defe41becc12b71898f0ddc2b62ff
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2023-03-29 21:29:13 -0400

    Rework base class tests for fixture.

--- tests/shapes/bases/test_line_collection.py
+++ tests/shapes/bases/test_line_collection.py
@@ -13,40 +13,43 @@ from data_morph.shapes.bases.line_collection import LineCollection
 class TestLineCollection:
     """Test the LineCollection class."""
 
-    line_collection = LineCollection(
-        [[0, 0], [0, 1]],
-        [[1, 0], [1, 1]],
-        [[10.5, 11.5], [11, 10]],
-    )
+    @pytest.fixture(scope='class')
+    def line_collection(self):
+        """An instance of LineCollection."""
+        return LineCollection(
+            [[0, 0], [0, 1]],
+            [[1, 0], [1, 1]],
+            [[10.5, 11.5], [11, 10]],
+        )
 
-    def test_distance_zero(self):
+    def test_distance_zero(self, line_collection):
         """Test the distance() method on points on lines in the collection."""
-        for point in itertools.chain(*self.line_collection.lines):
-            assert self.line_collection.distance(*point) == 0
+        for point in itertools.chain(*line_collection.lines):
+            assert line_collection.distance(*point) == 0
 
     @pytest.mark.parametrize(
         ['point', 'expected_distance'], [[(-1, 0), 1], [(-1, -1), 1.414214]], ids=str
     )
-    def test_distance_nonzero(self, point, expected_distance):
+    def test_distance_nonzero(self, line_collection, point, expected_distance):
         """Test the distance() method on points not on lines in the collection."""
-        assert pytest.approx(self.line_collection.distance(*point)) == expected_distance
+        assert pytest.approx(line_collection.distance(*point)) == expected_distance
 
     @pytest.mark.parametrize('line', [[(0, 0), (0, 0)], [(-1, -1), (-1, -1)]], ids=str)
-    def test_distance_to_small_line_magnitude(self, line):
+    def test_distance_to_small_line_magnitude(self, line_collection, line):
         """Test _distance_point_to_line() for small line magnitudes."""
-        distance = self.line_collection._distance_point_to_line((30, 50), line)
+        distance = line_collection._distance_point_to_line((30, 50), line)
         assert distance == 9999
 
-    def test_repr(self):
+    def test_repr(self, line_collection):
         """Test that the __repr__() method is working."""
         lines = r'\n        '.join(
             [r'\[\[\d+\.*\d*, \d+\.*\d*\], \[\d+\.*\d*, \d+\.*\d*\]\]']
-            * len(self.line_collection.lines)
+            * len(line_collection.lines)
         )
         assert (
             re.match(
                 (r'^<LineCollection>\n  lines=\n        ' + lines),
-                repr(self.line_collection),
+                repr(line_collection),
             )
             is not None
         )

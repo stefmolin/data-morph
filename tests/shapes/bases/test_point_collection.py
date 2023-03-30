5c572cac480e7726f62a69dbaa3eee7a874a3527
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2023-03-29 21:29:13 -0400

    Rework base class tests for fixture.

--- tests/shapes/bases/test_point_collection.py
+++ tests/shapes/bases/test_point_collection.py
@@ -12,25 +12,26 @@ from data_morph.shapes.bases.point_collection import PointCollection
 class TestPointCollection:
     """Test the PointCollection class."""
 
-    point_collection = PointCollection([0, 0], [20, 50])
+    @pytest.fixture(scope='class')
+    def point_collection(self):
+        """An instance of PointCollection."""
+        return PointCollection([0, 0], [20, 50])
 
-    def test_distance_zero(self):
+    def test_distance_zero(self, point_collection):
         """Test the distance() method on points in the collection."""
-        for point in self.point_collection.points:
-            assert self.point_collection.distance(*point) == 0
+        for point in point_collection.points:
+            assert point_collection.distance(*point) == 0
 
     @pytest.mark.parametrize(
         ['point', 'expected_distance'], [[(-1, 0), 1], [(-1, -1), 1.414214]]
     )
-    def test_distance_nonzero(self, point, expected_distance):
+    def test_distance_nonzero(self, point_collection, point, expected_distance):
         """Test the distance() method on points not in the collection."""
-        assert (
-            pytest.approx(self.point_collection.distance(*point)) == expected_distance
-        )
+        assert pytest.approx(point_collection.distance(*point)) == expected_distance
 
-    def test_repr(self):
+    def test_repr(self, point_collection):
         """Test that the __repr__() method is working."""
         assert (
-            re.match(r'^<PointCollection of \d* points>$', repr(self.point_collection))
+            re.match(r'^<PointCollection of \d* points>$', repr(point_collection))
             is not None
         )

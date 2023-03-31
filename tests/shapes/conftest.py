8aff0df5e2994472323ccac8e44cdee5a244e838
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2023-03-31 14:04:19 -0400

    Update scope on fixture used for shape tests.

--- tests/shapes/conftest.py
+++ tests/shapes/conftest.py
@@ -7,7 +7,10 @@ from data_morph.shapes.factory import ShapeFactory
 
 
 def pytest_generate_tests(metafunc):
-    """Parametrize test_distance() methods using distance_test_cases class attribute."""
+    """
+    Parametrize the test_distance() methods for shape tests
+    using the distance_test_cases class attribute on test classes.
+    """
     if metafunc.function.__name__ == 'test_distance':
         metafunc.parametrize(
             ['test_point', 'expected_distance'],
@@ -16,7 +19,7 @@ def pytest_generate_tests(metafunc):
         )
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='package')
 def shape_factory(sample_data):
     """Fixture for a ShapeFactory of sample data."""
     return ShapeFactory(Dataset('sample', sample_data))

9aba87a9a7e03cf97072f826445b9adf2eb178a8
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2023-03-30 21:56:25 -0400

    Use pytest_generate_tests to parametrize shape class tests using class attribute; move fixture used for shape tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,20 +5,11 @@ from pathlib import Path
 import pandas as pd
 import pytest
 
-from data_morph.data.dataset import Dataset
-from data_morph.shapes.factory import ShapeFactory
-
 
 @pytest.fixture(scope='session')
 def sample_data():
     """Fixture for the sample data."""
     return pd.DataFrame({'x': [10, 20, 30], 'y': [50, 50, 80]})
-
-
-@pytest.fixture(scope='session')
-def shape_factory(sample_data):
-    """Fixture for a ShapeFactory of sample data."""
-    return ShapeFactory(Dataset('sample', sample_data))
 
 
 @pytest.fixture(scope='session')

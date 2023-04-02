32a5fbadccd1f8f7c363269d92fdba0497ca3d30
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2023-04-01 21:00:45 -0400

    Add link to docs in CLI.

--- src/data_morph/cli.py
+++ src/data_morph/cli.py
@@ -79,7 +79,7 @@ def main(argv: Union[Sequence[str], None] = None) -> None:
         ),
         epilog=(
             'Source code available at https://github.com/stefmolin/data-morph.'
-            # ' Documentation is at TODO.'
+            ' Documentation is at https://stefmolin.github.io/data-morph.'
         ),
         formatter_class=argparse.RawDescriptionHelpFormatter,
     )

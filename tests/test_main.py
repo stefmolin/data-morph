9f0da2737de09a1788e7bcd67476da469920edae
Felipe Moreno <felipe@flpm.dev>
2024-05-22 13:41:27 -0400

    replace 'python' with sys.executable in CLI test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,7 @@
 """Test the __main__ module."""
 
 import subprocess
+import sys
 
 import pytest
 
@@ -10,5 +11,5 @@ pytestmark = pytest.mark.cli
 @pytest.mark.parametrize(['flag', 'return_code'], [['--version', 0], ['', 2]])
 def test_main_access_cli(flag, return_code):
     """Confirm that CLI can be accessed via __main__."""
-    result = subprocess.run(['python', '-m', 'data_morph', flag])
+    result = subprocess.run([sys.executable, '-m', 'data_morph', flag])
     assert result.returncode == return_code

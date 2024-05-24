8df8c3b8adc62ea2447f7fb747a5fdf49e2bd04d
Felipe Moreno <felipe@flpm.dev>
2024-05-24 14:19:19 -0400

    added tests for easing functions

--- tests/plotting/test_animation.py
+++ tests/plotting/test_animation.py
@@ -3,6 +3,7 @@
 import numpy as np
 import pytest
 
+from data_morph.plotting import animation
 from data_morph.plotting.animation import stitch_gif_animation
 from data_morph.plotting.static import plot
 
@@ -35,3 +36,56 @@ def test_frame_stitching(sample_data, tmp_path):
     animation_file = tmp_path / f'{start_shape}_to_{target_shape}.gif'
     assert animation_file.is_file()
     assert not (tmp_path / f'{start_shape}-to-{target_shape}-{frame}.png').is_file()
+
+
+@pytest.mark.parametrize(
+    ['ease_function', 'step', 'expected'],
+    [
+        ('linear', 0.1, 0.1),
+        ('linear', 0.5, 0.5),
+        ('linear', 0.9, 0.9),
+        ('ease_in_sine', 0.1, 0.012312),
+        ('ease_in_sine', 0.5, 0.292893),
+        ('ease_in_sine', 0.9, 0.843566),
+        ('ease_out_sine', 0.1, 0.156434),
+        ('ease_out_sine', 0.5, 0.707107),
+        ('ease_out_sine', 0.9, 0.987688),
+        ('ease_in_out_sine', 0.1, 0.024472),
+        ('ease_in_out_sine', 0.5, 0.5),
+        ('ease_in_out_sine', 0.9, 0.975528),
+        ('ease_in_out_quadratic', 0.1, 0.02),
+        ('ease_in_out_quadratic', 0.5, 0.5),
+        ('ease_in_out_quadratic', 0.9, 0.98),
+    ],
+)
+def test_easing_functions(ease_function, step, expected):
+    """Test that easing functions return expected values."""
+    ease_func = getattr(animation, ease_function)
+    assert round(ease_func(step), ndigits=6) == expected
+
+
+@pytest.mark.parametrize(
+    'invalid_step',
+    [
+        'string',
+        -1,
+        2,
+    ],
+)
+@pytest.mark.parametrize(
+    'ease_function',
+    [
+        'linear',
+        'ease_in_sine',
+        'ease_out_sine',
+        'ease_in_out_sine',
+        'ease_in_out_quadratic',
+    ],
+)
+def test_invalid_easing_step(ease_function, invalid_step):
+    """Test that an invalid step type will produce a ValueError when passed to an easing function."""
+    with pytest.raises(
+        ValueError, match='Step must be an integer or float, between 1 and 0.'
+    ):
+        ease_func = getattr(animation, ease_function)
+        ease_func(invalid_step)

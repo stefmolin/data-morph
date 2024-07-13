76f0670df6b8b79cbded2d10f7103b367d445f74
Sakurajima <sakurajimatop@gmail.com>
2024-07-13 15:07:51 +0200

    adding pi as starter shape

--- src/data_morph/data/loader.py
+++ src/data_morph/data/loader.py
@@ -51,6 +51,7 @@ class DataLoader:
         'Python': 'python.csv',
         'SDS': 'superdatascience.csv',
         'sheep': 'sheep.csv',
+        'pi':'pi.csv',
     }
     AVAILABLE_DATASETS = sorted(_DATASETS.keys(), key=str.lower)
     """list[str]: List of available built-in starter datasets,

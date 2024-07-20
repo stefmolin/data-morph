a52b25d918c27a7ddd6c4ef863f90d3abff0ca8c
Goran Jelic-Cizmek <goran.jelic-cizmek@epfl.ch>
2024-07-20 15:50:37 +0200

    Add `shifted_*` functions and `Statistics` class

--- src/data_morph/data/stats.py
+++ src/data_morph/data/stats.py
@@ -14,6 +14,221 @@ SummaryStatistics.__doc__ = (
 )
 
 
+def shifted_mean(
+    mean_old: float,
+    value_old: float,
+    value_new: float,
+    size: int,
+) -> float:
+    """
+    Return the shifted mean from the perturbed data.
+    """
+    return (mean_old - value_old / size) + value_new / size
+
+
+def shifted_var(
+    mean_old: float,
+    var_old: float,
+    value_old: float,
+    delta: float,
+    size: int,
+    *,
+    ddof: int = 0,
+) -> float:
+    """
+    Return the shifted covariance from the perturbed data.
+    """
+    return (
+        var_old
+        + 2 * delta * (value_old - mean_old) / (size - ddof)
+        + delta**2 * (1 / (size - ddof) - 1 / (size - ddof) / size)
+    )
+
+
+def shifted_stdev(*args, **kwargs):
+    return np.sqrt(shifted_var(*args, **kwargs))
+
+
+def shifted_corrcoef(
+    x_old: float,
+    y_old: float,
+    x_new: float,
+    y_new: float,
+    meanx_old: float,  # the mean <x>
+    meany_old: float,  # the mean <y>
+    xy_old: float,  # the mean <xy>
+    varx_old: float,  # the variance <x^2> - <x>^2
+    vary_old: float,  # the variance <y^2> - <y>^2
+    size: int,
+) -> float:
+    """
+    Return the shifted correlation coefficient of the perturbed data.
+    """
+    deltax = x_new - x_old
+    deltay = y_new - y_old
+
+    numerator = (
+        xy_old
+        + (deltax * y_old + deltay * x_old + deltax * deltay) / size
+        - shifted_mean(mean_old=meanx_old, value_old=x_old, value_new=x_new, size=size)
+        * shifted_mean(mean_old=meany_old, value_old=y_old, value_new=y_new, size=size)
+    )
+
+    denominator = np.sqrt(
+        shifted_var(
+            mean_old=meanx_old,
+            var_old=varx_old,
+            value_old=x_old,
+            delta=deltax,
+            size=size,
+        )
+        * shifted_var(
+            mean_old=meany_old,
+            var_old=vary_old,
+            value_old=y_old,
+            delta=deltay,
+            size=size,
+        )
+    )
+
+    return numerator / denominator
+
+
+class Statistics:
+    def __init__(self, x: Iterable[Number], y: Iterable[Number]):
+        if len(x) != len(y):
+            raise ValueError('The two datasets should have the same size')
+
+        self._x = np.copy(x)
+        self._y = np.copy(y)
+        self._size = len(self._x)
+        self._x_mean = np.mean(self._x)
+        self._y_mean = np.mean(self._y)
+        self._x_var = np.var(self._x, ddof=0)
+        self._x_stdev = np.sqrt(self._x_var)
+        self._y_var = np.var(self._y, ddof=0)
+        self._y_stdev = np.sqrt(self._y_var)
+        self._corrcoef = np.corrcoef(self._x, self._y)[0, 1]
+        self._xy_mean = np.mean(self._x * self._y)
+
+    @property
+    def x_mean(self):
+        """
+        Return the mean of the ``x`` data.
+        """
+        return self._x_mean
+
+    @property
+    def y_mean(self):
+        """
+        Return the mean of the ``y`` data.
+        """
+        return self._y_mean
+
+    @property
+    def x_stdev(self):
+        """
+        Return the std of the ``x`` data.
+        """
+        return self._x_stdev
+
+    @property
+    def y_stdev(self):
+        """
+        Return the std of the ``y`` data.
+        """
+        return self._y_stdev
+
+    @property
+    def corrcoef(self):
+        """
+        Return the correlation coefficient of the ``x`` and ``y`` data.
+        """
+        return self._corrcoef
+
+    def __len__(self):
+        """
+        Return the size of the dataset.
+        """
+        return len(self._x)
+
+    def perturb(
+        self,
+        index: int,
+        deltax: float,
+        deltay: float,
+        *,
+        update: bool = False,
+    ) -> SummaryStatistics:
+        """
+        Perturb a single point and return the new ``SummaryStatistics``.
+        """
+        x_mean = shifted_mean(
+            mean_old=self.x_mean,
+            value_old=self._x[index],
+            value_new=self._x[index] + deltax,
+            size=len(self),
+        )
+        y_mean = shifted_mean(
+            mean_old=self.y_mean,
+            value_old=self._y[index],
+            value_new=self._y[index] + deltay,
+            size=len(self),
+        )
+
+        x_var = shifted_var(
+            mean_old=self.x_mean,
+            var_old=self._x_var,
+            value_old=self._x[index],
+            delta=deltax,
+            size=len(self),
+        )
+
+        y_var = shifted_var(
+            mean_old=self.y_mean,
+            var_old=self._y_var,
+            value_old=self._y[index],
+            delta=deltay,
+            size=len(self),
+        )
+
+        corrcoef = shifted_corrcoef(
+            x_old=self._x[index],
+            y_old=self._y[index],
+            x_new=self._x[index] + deltax,
+            y_new=self._y[index] + deltay,
+            meanx_old=self.x_mean,
+            meany_old=self.y_mean,
+            xy_old=self._xy_mean,
+            varx_old=self._x_var,
+            vary_old=self._y_var,
+            size=len(self),
+        )
+
+        if update:
+            self._x_mean = x_mean
+            self._y_mean = y_mean
+            self._x_var = x_var
+            self._y_var = y_var
+            self._x_stdev = np.sqrt(x_var)
+            self._y_stdev = np.sqrt(y_var)
+            self._corrcoef = corrcoef
+            self._xy_mean += (
+                deltax * self._y[index] + deltay * self._x[index] + deltax * deltay
+            ) / len(self)
+
+            self._x[index] += deltax
+            self._y[index] += deltay
+
+        return SummaryStatistics(
+            x_mean,
+            y_mean,
+            np.sqrt(x_var),
+            np.sqrt(y_var),
+            corrcoef,
+        )
+
+
 def get_values(x: Iterable[Number], y: Iterable[Number]) -> SummaryStatistics:
     """
     Calculate the summary statistics for the given set of points.
--- tests/data/test_stats.py
+++ tests/data/test_stats.py
@@ -1,9 +1,15 @@
 """Test the stats module."""
 
 import numpy as np
+from numpy.testing import assert_allclose, assert_equal
 
 from data_morph.data.loader import DataLoader
-from data_morph.data.stats import get_values
+from data_morph.data.stats import (
+    get_values,
+    shifted_mean,
+    shifted_var,
+    shifted_corrcoef,
+)
 
 
 def test_stats():
@@ -18,3 +24,161 @@ def test_stats():
     assert stats.x_stdev == data.x.std()
     assert stats.y_stdev == data.y.std()
     np.allclose(stats.correlation, data.corr().x.y)
+
+
+def test_new_mean():
+    data = DataLoader.load_dataset('dino').df
+
+    # make sure if we don't do anything to the data that we retrieve the same results
+    x = data['x'].to_numpy()
+
+    assert_equal(np.mean(x), shifted_mean(np.mean(x), x[0], x[0], len(x)))
+
+    # we want to test both very large and very small displacements
+    for scale in [0.1, 10]:
+        x_old = data['x'].to_numpy()
+        y_old = data['y'].to_numpy()
+
+        # scaling the data
+        x_old /= np.max(np.abs(x_old))
+        y_old /= np.max(np.abs(y_old))
+
+        x_new = np.copy(x_old)
+        y_new = np.copy(y_old)
+
+        rng = np.random.default_rng(42)
+
+        for _ in range(100_000):
+            row = rng.integers(0, len(x_old))
+            jitter_x, jitter_y = rng.normal(loc=0, scale=scale, size=2)
+
+            x_new[row] += jitter_x
+            y_new[row] += jitter_y
+
+            meanx = np.mean(x_new)
+            new_meanx = shifted_mean(np.mean(x_old), x_old[row], x_new[row], len(x_old))
+
+            meany = np.mean(y_new)
+            new_meany = shifted_mean(np.mean(y_old), y_old[row], y_new[row], len(y_old))
+
+            assert_allclose(meanx, new_meanx)
+            assert_allclose(meany, new_meany)
+
+            x_old = np.copy(x_new)
+            y_old = np.copy(y_new)
+
+
+def test_new_var():
+    data = DataLoader.load_dataset('dino').df
+
+    # make sure if we don't do anything to the data that we retrieve the same results
+    x = data['x'].to_numpy()
+
+    assert_equal(np.var(x, ddof=0), shifted_var(np.mean(x), np.var(x), x[0], 0, len(x)))
+
+    # we want to test both very large and very small displacements
+    for scale in [0.1, 10]:
+        x_old = data['x'].to_numpy()
+        y_old = data['y'].to_numpy()
+
+        # scaling the data
+        x_old /= np.max(np.abs(x_old))
+        y_old /= np.max(np.abs(y_old))
+
+        x_new = np.copy(x_old)
+        y_new = np.copy(y_old)
+
+        rng = np.random.default_rng(42)
+
+        for _ in range(100_000):
+            row = rng.integers(0, len(x_old))
+            jitter_x, jitter_y = rng.normal(loc=0, scale=scale, size=2)
+
+            x_new[row] += jitter_x
+            y_new[row] += jitter_y
+
+            varx = np.var(x_new)
+            new_varx = shifted_var(
+                np.mean(x_old), np.var(x_old), x_old[row], jitter_x, len(x_old)
+            )
+
+            vary = np.var(y_new)
+            new_vary = shifted_var(
+                np.mean(y_old), np.var(y_old), y_old[row], jitter_y, len(y_old)
+            )
+
+            assert_allclose(varx, new_varx)
+            assert_allclose(vary, new_vary)
+
+            x_old = np.copy(x_new)
+            y_old = np.copy(y_new)
+
+
+def test_new_corrcoef():
+    data = DataLoader.load_dataset('dino').df
+
+    # make sure if we don't do anything to the data that we retrieve the same results
+    x = data['x'].to_numpy()
+    y = data['y'].to_numpy()
+    corrcoef = np.corrcoef(x, y)[0, 1]
+    row = 0
+    new_corrcoef = shifted_corrcoef(
+        x_old=x[row],
+        y_old=y[row],
+        x_new=x[row],
+        y_new=y[row],
+        meanx_old=np.mean(x),
+        meany_old=np.mean(y),
+        xy_old=np.mean(x * y),
+        varx_old=np.var(x),
+        vary_old=np.var(y),
+        size=len(x),
+    )
+
+    corrcoef_by_hand = np.cov(x, y, ddof=0) / np.sqrt(np.var(x) * np.var(y))
+
+    assert_allclose(corrcoef, corrcoef_by_hand[0, 1])
+    assert_allclose(corrcoef_by_hand[0, 1], new_corrcoef)
+
+    # we want to test both very large and very small displacements
+    for scale in [0.1, 10]:
+        x_old = data['x'].to_numpy()
+        y_old = data['y'].to_numpy()
+
+        # scaling the data
+        x_old /= np.max(np.abs(x_old))
+        y_old /= np.max(np.abs(y_old))
+
+        x_new = np.copy(x_old)
+        y_new = np.copy(y_old)
+
+        rng = np.random.default_rng(42)
+
+        for _ in range(100_000):
+            row = rng.integers(0, len(x_old))
+            jitter_x, jitter_y = rng.normal(loc=0, scale=scale, size=2)
+
+            x_new[row] += jitter_x
+            y_new[row] += jitter_y
+
+            # corrcoef = np.corrcoef(x_new, y_new)[0, 1]
+            corrcoef = (
+                np.cov(x_new, y_new, ddof=0) / np.sqrt(np.var(x_new) * np.var(y_new))
+            )[0, 1]
+            new_corrcoef = shifted_corrcoef(
+                x_old=x_old[row],
+                y_old=y_old[row],
+                x_new=x_new[row],
+                y_new=y_new[row],
+                meanx_old=np.mean(x_old),
+                meany_old=np.mean(y_old),
+                xy_old=np.mean(x_old * y_old),
+                varx_old=np.var(x_old),
+                vary_old=np.var(y_old),
+                size=len(x_old),
+            )
+
+            assert_allclose(corrcoef, new_corrcoef)
+
+            x_old = np.copy(x_new)
+            y_old = np.copy(y_new)

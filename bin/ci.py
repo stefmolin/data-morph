d6136d390b80f9ec2231690f2a79e4e168b86fb9
Daniel Schaefer <git@danielschaefer.me>
2024-09-23 16:12:27 +0800

    review, please squash
    Signed-off-by: Daniel Schaefer <git@danielschaefer.me>

--- bin/ci.py
+++ bin/ci.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
+"""
+Pass in the filenames that changed and it'll tell you the arguments of datasets and shapes.
+See examples below
+
+$ python bin/ci.py src/data_morph/shapes/circles.py
+bullseye circle rings
+
+$ python bin/ci.py src/data_morph/shapes/bases/line_collection.py
+high_lines h_lines slant_down slant_up v_lines wide_lines x diamond rectangle star
+
+ python bin/ci.py src/data_morph/data/starter_shapes/superdatascience.csv
+SDS
+"""
 from data_morph.shapes.factory import ShapeFactory
 from data_morph.data.loader import DataLoader
 import sys
-from inspect import getmro
 from os.path import basename
 
 new_paths = sys.argv[1:]
@@ -22,13 +34,13 @@ for shape, c in ShapeFactory._SHAPE_MAPPING.items():
         # Find the class and all parent classes and get their module name
         # We get the module name because it ends in the python file without .py extension
         # To make it easy to compare, we just add the extension onto the end
-        parents = [x.__module__ for x in getmro(c)]
+        parents = [x.__module__ for x in c.__mro__]
         all_modules = parents + [c.__module__]
-        all_modules = [f"{x}.py" for x in all_modules]
+        all_modules = [f'{x}.py' for x in all_modules]
 
         for module in all_modules:
             if module.endswith(new_file):
                 args.append(shape)
                 break
 
-print(" ".join(args))
+print(' '.join(args))

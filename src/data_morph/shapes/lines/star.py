537de234dd627ba570b04da987ec9402bf8cd58c
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2024-11-25 11:32:09 +1100

    Move shapes into submodules and individual files in most cases

---
+++ src/data_morph/shapes/lines/star.py
@@ -0,0 +1,55 @@
+"""Star shape."""
+
+from ...data.dataset import Dataset
+from ..bases.line_collection import LineCollection
+
+
+class Star(LineCollection):
+    """
+    Class for the star shape.
+
+    .. plot::
+       :scale: 75
+       :caption:
+            This shape is generated using the panda dataset.
+
+        import matplotlib.pyplot as plt
+        from data_morph.data.loader import DataLoader
+        from data_morph.shapes.polygons import Star
+
+        _ = Star(DataLoader.load_dataset('panda')).plot()
+
+    Parameters
+    ----------
+    dataset : Dataset
+        The starting dataset to morph into other shapes.
+    """
+
+    def __init__(self, dataset: Dataset) -> None:
+        bounds = dataset.data_bounds.clone()
+        bounds.align_aspect_ratio()
+
+        x_bounds = bounds.x_bounds
+        y_bounds = bounds.y_bounds
+
+        xmin, xmax = x_bounds
+        ymin, ymax = y_bounds
+
+        x_range = x_bounds.range
+        y_range = y_bounds.range
+
+        pts = [
+            [xmin, ymin + y_range * 0.625],
+            [xmin + x_range * 0.375, ymin + y_range * 0.625],
+            [xmin + x_range * 0.5, ymax],
+            [xmin + x_range * 0.625, ymin + y_range * 0.625],
+            [xmax, ymin + y_range * 0.625],
+            [xmin + x_range * 0.6875, ymin + y_range * 0.375],
+            [xmin + x_range * 0.8125, ymin],
+            [xmin + x_range * 0.5, ymin + y_range * 0.25],
+            [xmin + x_range * 0.1875, ymin],
+            [xmin + x_range * 0.3125, ymin + y_range * 0.375],
+            [xmin, ymin + y_range * 0.625],
+        ]
+
+        super().__init__(*list(zip(pts[:-1], pts[1:])))
---
+++ src/data_morph/shapes/lines/x_lines.py
@@ -0,0 +1,34 @@
+"""X lines shape."""
+
+from ...data.dataset import Dataset
+from ..bases.line_collection import LineCollection
+
+
+class XLines(LineCollection):
+    """
+    Class for the X shape consisting of two crossing, perpendicular lines.
+
+    .. plot::
+       :scale: 75
+       :caption:
+            This shape is generated using the panda dataset.
+
+        from data_morph.data.loader import DataLoader
+        from data_morph.shapes.lines import XLines
+
+        _ = XLines(DataLoader.load_dataset('panda')).plot()
+
+    Parameters
+    ----------
+    dataset : Dataset
+        The starting dataset to morph into other shapes.
+    """
+
+    def __init__(self, dataset: Dataset) -> None:
+        xmin, xmax = dataset.morph_bounds.x_bounds
+        ymin, ymax = dataset.morph_bounds.y_bounds
+
+        super().__init__([[xmin, ymin], [xmax, ymax]], [[xmin, ymax], [xmax, ymin]])
+
+    def __str__(self) -> str:
+        return 'x'

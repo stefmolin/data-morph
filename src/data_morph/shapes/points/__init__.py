7b92d52002de129419fb3be36624ea53048fd008
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2025-02-11 22:07:27 -0500

    Add figure eight target shape

--- src/data_morph/shapes/points/__init__.py
+++ src/data_morph/shapes/points/__init__.py
@@ -2,6 +2,7 @@
 
 from .club import Club
 from .dots_grid import DotsGrid
+from .figure_eight import FigureEight
 from .heart import Heart
 from .parabola import DownParabola, LeftParabola, RightParabola, UpParabola
 from .scatter import Scatter
@@ -12,6 +13,7 @@ __all__ = [
     'Club',
     'DotsGrid',
     'DownParabola',
+    'FigureEight',
     'Heart',
     'LeftParabola',
     'RightParabola',

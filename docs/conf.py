6c1f0f6f085b2728020eb88fde2b173edef534b3
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2025-02-11 22:14:47 -0500

    Remove show rST source link on docs

--- docs/conf.py
+++ docs/conf.py
@@ -48,6 +48,7 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+html_show_sourcelink = False
 
 # -- autosectionlabel ---------------------------------------------------------
 

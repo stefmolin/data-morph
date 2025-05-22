502372b60e255efa0fbc6f03de5d5474a58fef31
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2025-05-21 21:29:28 -0400

    Update tests

--- tests/plotting/test_static.py
+++ tests/plotting/test_static.py
@@ -1,5 +1,7 @@
 """Test the static module."""
 
+import matplotlib.pyplot as plt
+import numpy as np
 import pytest
 
 from data_morph.plotting.static import plot
@@ -8,16 +10,23 @@ pytestmark = pytest.mark.plotting
 
 
 @pytest.mark.parametrize(
-    ('file_path', 'with_median'),
+    ('file_path', 'with_median', 'classic'),
     [
-        ('test_plot.png', False),
-        (None, True),
-        (None, False),
+        ('test_plot.png', False, True),
+        (None, True, True),
+        (None, False, True),
+        (None, True, False),
+        (None, False, False),
     ],
 )
-def test_plot(sample_data, tmp_path, file_path, with_median):
+def test_plot(sample_data, tmp_path, file_path, with_median, classic):
     """Test static plot creation."""
     bounds = (-5.0, 105.0)
+
+    marginals = (
+        None if classic else (np.histogram(sample_data.x), np.histogram(sample_data.y))
+    )
+
     if file_path:
         save_to = tmp_path / 'another-level' / file_path
 
@@ -28,6 +37,7 @@ def test_plot(sample_data, tmp_path, file_path, with_median):
             save_to=save_to,
             decimals=2,
             with_median=with_median,
+            marginals=marginals,
         )
         assert save_to.is_file()
 
@@ -39,6 +49,7 @@ def test_plot(sample_data, tmp_path, file_path, with_median):
             save_to=None,
             decimals=2,
             with_median=with_median,
+            marginals=marginals,
         )
 
         # confirm that the stylesheet was used
@@ -52,3 +63,10 @@ def test_plot(sample_data, tmp_path, file_path, with_median):
         expected_stats = 7 if with_median else 5
         expected_texts = 2 * expected_stats  # label and the number
         assert len(ax.texts) == expected_texts
+
+        # if marginals should be there, check for two inset Axes
+        expected_insets = 0 if classic else 2
+        inset_axes = [
+            child for child in ax.get_children() if isinstance(child, plt.Axes)
+        ]
+        assert len(inset_axes) == expected_insets
